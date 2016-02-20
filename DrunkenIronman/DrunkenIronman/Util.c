1ef8497b5a3277fa07a0e889e8b84deb0101168d
Michael Bikovitsky <moshe.biko@gmail.com>
2016-02-20 22:13:36 +0200

    Implement reading resources

--- DrunkenIronman/DrunkenIronman/Util.c
+++ DrunkenIronman/DrunkenIronman/Util.c
@@ -1,6 +1,8 @@
 #include <windows.h>
 #include <tchar.h>
 
+#include "Util.h"
+
 
 typedef
 BOOL
@@ -51,5 +53,123 @@ UTIL_IsWow64Process(
 	hrResult = S_OK;
 
 lblCleanup:
+	return hrResult;
+}
+
+HRESULT
+UTIL_ReadResource(
+	_In_										HMODULE	hModule,
+	_In_										PCTSTR	pszResourceName,
+	_In_										PCTSTR	pszResourceType,
+	_In_										WORD	eLanguage,
+	_Outptr_result_bytebuffer_(*pcbResource)	PVOID *	ppvResource,
+	_Out_										PDWORD	pcbResource
+)
+{
+	HRESULT	hrResult		= E_FAIL;
+	HRSRC	hResInfo		= NULL;
+	HGLOBAL	hResData		= NULL;
+	PVOID	pvResource		= NULL;
+	DWORD	cbResource		= 0;
+	PVOID	pvResourceCopy	= NULL;
+
+	if ((NULL == hModule) ||
+		(NULL == ppvResource) ||
+		(NULL == pcbResource))
+	{
+		hrResult = E_INVALIDARG;
+		goto lblCleanup;
+	}
+
+	hResInfo = FindResourceEx(hModule, pszResourceType, pszResourceName, eLanguage);
+	if (NULL == hResInfo)
+	{
+		hrResult = HRESULT_FROM_WIN32(GetLastError());
+		goto lblCleanup;
+	}
+
+	hResData = LoadResource(hModule, hResInfo);
+	if (NULL == hResData)
+	{
+		hrResult = HRESULT_FROM_WIN32(GetLastError());
+		goto lblCleanup;
+	}
+
+	pvResource = LockResource(hResData);
+	if (NULL == pvResource)
+	{
+		hrResult = HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND);
+		goto lblCleanup;
+	}
+
+	cbResource = SizeofResource(hModule, hResInfo);
+	if (0 == cbResource)
+	{
+		hrResult = HRESULT_FROM_WIN32(GetLastError());
+		goto lblCleanup;
+	}
+
+	pvResourceCopy = HEAPALLOC(cbResource);
+	if (NULL == pvResourceCopy)
+	{
+		hrResult = HRESULT_FROM_WIN32(GetLastError());
+		goto lblCleanup;
+	}
+
+	MoveMemory(pvResourceCopy, pvResource, cbResource);
+
+	// Transfer ownership:
+	*ppvResource = pvResourceCopy;
+	pvResourceCopy = NULL;
+	*pcbResource = cbResource;
+
+	hrResult = S_OK;
+
+lblCleanup:
+	HEAPFREE(pvResourceCopy);
+
+	return hrResult;
+}
+
+HRESULT
+UTIL_ReadResourceFromFile(
+	_In_										PCTSTR	pszResourceModulePath,
+	_In_										PCTSTR	pszResourceName,
+	_In_										PCTSTR	pszResourceType,
+	_In_										WORD	eLanguage,
+	_Outptr_result_bytebuffer_(*pcbResource)	PVOID *	ppvResource,
+	_Out_										PDWORD	pcbResource
+)
+{
+	HRESULT	hrResult		= E_FAIL;
+	HMODULE	hResourceModule	= NULL;
+
+	if ((NULL == pszResourceModulePath) ||
+		(NULL == ppvResource) ||
+		(NULL == pcbResource))
+	{
+		hrResult = E_INVALIDARG;
+		goto lblCleanup;
+	}
+
+	hResourceModule = LoadLibraryEx(pszResourceModulePath, NULL, LOAD_LIBRARY_AS_DATAFILE);
+	if (NULL == hResourceModule)
+	{
+		hrResult = HRESULT_FROM_WIN32(GetLastError());
+		goto lblCleanup;
+	}
+
+	hrResult = UTIL_ReadResource(hResourceModule,
+								 pszResourceName,
+								 pszResourceType,
+								 eLanguage,
+								 ppvResource,
+								 pcbResource);
+
+	// Keep last status
+
+lblCleanup:
+	CLOSE(hResourceModule, FreeLibrary);
+
 	return hrResult;
 }
--- DrunkenIronman/DrunkenIronman/Util.h
+++ DrunkenIronman/DrunkenIronman/Util.h
@@ -4,7 +4,41 @@
 #include <tchar.h>
 
 
+#define HEAPALLOC(cbSize) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, (cbSize))
+#define HEAPFREE(pvMemory) HeapFree(GetProcessHeap(), 0, (pvMemory))
+
+#define CLOSE_TO_VALUE(object, pfnDestructor, value)	\
+	if ((value) != (object))							\
+	{													\
+		(VOID)(pfnDestructor)(object);					\
+		(object) = (value);								\
+	}
+#define CLOSE(object, pfnDestructor) CLOSE_TO_VALUE((object), (pfnDestructor), NULL)
+#define CLOSE_HANDLE(hObject) CLOSE((hObject), CloseHandle)
+#define CLOSE_FILE_HANDLE(hFile) CLOSE_TO_VALUE((hObject), CloseHandle, INVALID_HANDLE_VALUE)
+
+
 HRESULT
 UTIL_IsWow64Process(
 	_Out_	PBOOL	pbWow64Process
+);
+
+HRESULT
+UTIL_ReadResource(
+	_In_										HMODULE	hModule,
+	_In_										PCTSTR	pszResourceName,
+	_In_										PCTSTR	pszResourceType,
+	_In_										WORD	eLanguage,
+	_Outptr_result_bytebuffer_(*pcbResource)	PVOID *	ppvResource,
+	_Out_										PDWORD	pcbResource
+);
+
+HRESULT
+UTIL_ReadResourceFromFile(
+	_In_										PCTSTR	pszResourceModulePath,
+	_In_										PCTSTR	pszResourceName,
+	_In_										PCTSTR	pszResourceType,
+	_In_										WORD	eLanguage,
+	_Outptr_result_bytebuffer_(*pcbResource)	PVOID *	ppvResource,
+	_Out_										PDWORD	pcbResource
 );
